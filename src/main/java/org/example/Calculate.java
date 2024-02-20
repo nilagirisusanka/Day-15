598438344524ff120858273151942b2aa3cb3e03
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-20 10:55:52 +0530

    Calculate class is created

--- src/main/java/org/example/Calculate.java
+++ src/main/java/org/example/Calculate.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Calculate {
+    public int sum(int a,int b){
+        return a+b;
+    }
     public int difference(int a,int b){
         return a-b;
         }
