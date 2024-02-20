1eb2bd3bd3a64cce61d81516a666ab215359a1bd
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-20 11:23:04 +0530

    Commit the change in test branch

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -3,8 +3,7 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Assignment {
-    public  String print() {
-        return "hai from the test branch";
+    public String print() {
+        return "hai from the test  branch";
     }
-
 }
