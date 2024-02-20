ba3ab09c4afd7225d01727b9d7d52da17ccd97e6
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-20 11:24:04 +0530

    Commit the change in new branch

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -3,9 +3,7 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Assignment {
-    public  String print() {
-        return "hai from the test child branch";
-    }
+    
     public int sum(int a,int b){
         return a+b;
     }
