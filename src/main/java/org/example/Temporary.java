c5f3df9f9bcc627409c45d7f0cf0486412446edd
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-21 09:30:20 +0530

    Add the Temporary class changes to Example Branch

--- src/main/java/org/example/Temporary.java
+++ src/main/java/org/example/Temporary.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Temporary {
+<<<<<<< Updated upstream
     public  void  wish(){
+=======
+    public  String wish(){
+     return "have a nice day";
+>>>>>>> Stashed changes
     }
 }
